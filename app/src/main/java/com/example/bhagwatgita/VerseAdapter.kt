a72cf3ba1de32b5846b22393dc0ccbc92e929e18
Suryash <joblesscoder2468@gmail.com>
2023-08-08 23:10:51 +0530

    worked on the liked verse feature and play audio feauture, which works right now, only using next and prev button

--- app/src/main/java/com/example/bhagwatgita/VerseAdapter.kt
+++ app/src/main/java/com/example/bhagwatgita/VerseAdapter.kt
@@ -1,6 +1,9 @@
 package com.example.bhagwatgita
 
+import android.content.Context.MODE_PRIVATE
 import android.content.Intent
+import android.media.MediaPlayer
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,17 +11,14 @@ import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
-import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
-import androidx.viewpager.widget.PagerAdapter
-import androidx.viewpager.widget.ViewPager
 import androidx.viewpager2.widget.ViewPager2
-import com.example.bhagwatgita.models.GetAllVerses
 import com.example.bhagwatgita.models.GetAllVersesItem
 import com.example.bhagwatgita.models.TranslationsItemx
 import startSound
 
 class VerseAdapter(private var verses: List<GetAllVersesItem>, private var view_pager2 : ViewPager2) : RecyclerView.Adapter<VerseAdapter.Pager2ViewHolder>() {
+    var mMediaPlayer: MediaPlayer? = null
 
     inner class Pager2ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
         val chapXVerse= itemView.findViewById<TextView>(R.id.chapXverse)
@@ -29,6 +29,9 @@ class VerseAdapter(private var verses: List<GetAllVersesItem>, private var view_
         val backBtn= itemView.findViewById<ImageButton>(R.id.back)
         val nextBtn= itemView.findViewById<ImageButton>(R.id.next)
         val prevBtn= itemView.findViewById<ImageButton>(R.id.prev)
+        val likeBtn= itemView.findViewById<ImageButton>(R.id.liked)
+        val playBtn= itemView.findViewById<ImageButton>(R.id.play_verse)
+        val ctx= itemView.context
 
 //        var x= "1"
 
@@ -50,48 +53,109 @@ class VerseAdapter(private var verses: List<GetAllVersesItem>, private var view_
                 }
 
             }
-            versePlayer.setOnClickListener {
-                startSound( "1/1.mp3", itemView.context)
 
-            }
             backBtn.setOnClickListener{
                 val i= Intent(itemView.context, ChapterListActivity::class.java)
                 itemView.context.startActivity(i)
             }
 
 
+
         }
 
     }
 
+    private fun handleMusic(itemView: View, playBtn: ImageButton?, verse: Int?) {
+        if(mMediaPlayer!= null && mMediaPlayer!!.isPlaying){
+            mMediaPlayer?.pause()
+            playBtn?.setImageResource(R.drawable.ic_play_music)
+        }
+        else{
+            if(mMediaPlayer== null){
+                mMediaPlayer= MediaPlayer.create(itemView.context, itemView.context.resources.getIdentifier("x${verse}", "raw", itemView.context.packageName))
+                mMediaPlayer!!.start()
+            }
+            else mMediaPlayer!!.start()
+            playBtn?.setImageResource(R.drawable.ic_pause)
+
+        }
+    }
 
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): VerseAdapter.Pager2ViewHolder {
-        return Pager2ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.fragment_verse_content, parent, false))
+        val v = Pager2ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.fragment_verse_content, parent, false))
+        return v
+    }
+
+    private fun getLikedStatus(chap: Int?, verse: Int?): Any? {
+        return "unliked"
     }
 
     override fun onBindViewHolder(holder: Pager2ViewHolder, position: Int) {
+        val chap= verses.get(position).chapter_number
+        val verse= verses.get(position).verse_number
+
+
         holder.verse_sanskrit.text= verses.get(position).text
         holder.verse_translation.text= getHindiTranslation(verses.get(position).translations) ?:"Not found"
-        holder.chapXVerse.text= "Chapter ${verses.get(position).chapter_number.toString()} Verse "
+        holder.chapXVerse.text= "Chapter ${chap.toString()} Verse "
         holder.editText.setText((position+1).toString())
+        holder.likeBtn.tag= getLikedStatus(chap, verse)
+        val pref= holder.ctx.getSharedPreferences("pref", MODE_PRIVATE)
+        val editor= pref.edit()
+        editor.putInt("arr2", 6)
+        editor.apply()
+        Log.i("test", pref.all.toString())
+
+
         holder.nextBtn.setOnClickListener {
             view_pager2.setCurrentItem(position+1 ,true)
+            stopAudio()
+
         }
+
         holder.prevBtn.setOnClickListener {
             view_pager2.setCurrentItem(position-1 ,true)
+            stopAudio()
+        }
+        holder.likeBtn.setOnClickListener {
+            if(holder.likeBtn.tag== "liked"){
+                holder.likeBtn.setImageResource(R.drawable.ic_unliked)
+//                removeFromFav(chap, verse)
+                holder.likeBtn.tag= "unliked"
+                Toast.makeText(holder.ctx, "Removed From Favourites", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                holder.likeBtn.setImageResource(R.drawable.ic_liked)
+//                addToFav(chap, verse)
+                holder.likeBtn.tag= "liked"
+                Toast.makeText(holder.ctx, "Added To Favourites", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+        holder.versePlayer.setOnClickListener {
+            handleMusic(holder.itemView, holder.playBtn, verse)
         }
 
 
+    }
+
+    private fun stopAudio() {
+        if(mMediaPlayer!= null){
+            if(mMediaPlayer!!.isPlaying) mMediaPlayer?.pause()
+            mMediaPlayer= null
+
+        }
     }
 
     private fun getHindiTranslation(translations: List<TranslationsItemx?>?): CharSequence? {
         translations?.forEach{
             if(it?.language == "hindi") return it.description
         }
+
         return translations?.get(0)?.description
     }
 
